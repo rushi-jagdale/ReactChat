067661ffaf68c75bb13f2f64567ab20d4acf63f0
rushi.jagdale <rjcom99@gmail.com>
2021-03-06 21:54:29 +0530

    add src files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ChatEngine
+    height="100vh"
+    projectID="a463e2f3-6a8d-4b1d-9759-fdc237347372"
+    userName="Rushi"
+    userSecret="1234" 
+    renderChatFeed ={(chatAppProps) => <ChatFeed {...chatAppProps} /> } 
+    >
+            
+     </ChatEngine>
     </div>
   );
 }
